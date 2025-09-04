e40131a29912120952186009a5c751d00cf7c59c
VinujaJayasundara <137858089+VinujaJayasundara@users.noreply.github.com>
2025-09-04 13:21:08 +0530

    first commit

---
+++ Variable.js
@@ -0,0 +1,11 @@
+//variables
+// 1 declaration let x;
+// 2 assignment x = 100;
+
+let name = "vinuja Jayasundata";
+let age = 23;
+let student = true;
+
+document.getElementById("p2").textContent = `your Name is ${name}`;   
+document.getElementById("p3").textContent = age;     
+document.getElementById("p4").textContent = student;
---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log("Hello World");
+
+//window.alert("This is an alert");
+//alert("This is also an alert");
+//window.alert("I really like pizza");
+// comments for developers
+document.getElementById("myh1").textContent = "Hello";
+document.getElementById("myP").textContent = "This is a paragraph";
